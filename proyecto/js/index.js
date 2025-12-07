16fc8a53c2f30e1b2de85b783fec1e10dee3d8bb
vielkab <vielkaborja@gmail.com>
2025-12-07 16:49:59 -0500

    uso de ficheros json

--- proyecto/js/index.js
+++ proyecto/js/index.js
@@ -1,38 +1,3 @@
-// usuarios simulados
-/*const usuarios = [
-    { correo: "e1234567890@live.uleam.edu.ec", password: "12345", rol: "usuario" },
-    { correo: "mariaquiroz@uleam.edu.ec", password: "admin123", rol: "admin" }
-];
-
-function validarformulario() {
-    console.log('Enviando formulario...');
-    const usuario = document.getElementById("correo").value;
-    const password = document.getElementById("password").value;
-
-    if (!usuario || !password) {
-        alert('Debe llenar todos los campos.')
-        return false
-    } 
-
-    const usuarioValidado = usuarios.find(u => u.correo === usuario && u.password === password);
-
-    if (usuarioValidado) {
-        if (usuarioValidado.rol === "usuario") {
-            alert("Bienvenido usuario");
-            window.location.href = "principal.html";
-            return false;
-        } else {
-            alert("Bienvenido administrador");
-            window.location.href = "principaladmin.html";
-            return false;
-        }
-    } else {
-        alert("Usuario o contraseña incorrectos");
-        return false;
-    }
-}*/
-
-
 function validarformulario() {
 
     // Obtener valores del formulario
@@ -82,5 +47,5 @@ function validarformulario() {
             alert("Error cargando usuarios. Verifica la ruta del JSON.");
         });
 
-    return false; // evitar recarga del form
+    return false;
 }
